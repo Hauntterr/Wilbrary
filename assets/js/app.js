0bcfe6377df27cec2406c044d78e4987725960b3
Hauntterr <135064464+Hauntterr@users.noreply.github.com>
2023-11-29 19:05:48 -0300

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,26 @@
+﻿// Função para verificar se o user está autenticado
+
+function checkAuthentication() {
+    //Verifique se o user está autenticado comparando com um valor no localStorage
+    const IsAuthenticated = localStorage.getItem('authenticated');
+
+    // Se isAuthenticated for verdadeiro, o user está autenticado
+    return IsAuthenticated === 'true';
+}
+
+// Verificar a autenticação antes de permitir o acesso á dashboard
+window.addEventListener('DOMContentLoaded', () => {
+    if (!checkAuthentication()) {
+        //Se o user não estiver autenticado, redirecione para a página login
+        window.location.href = 'login.html';
+    }
+});
+
+function menu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
